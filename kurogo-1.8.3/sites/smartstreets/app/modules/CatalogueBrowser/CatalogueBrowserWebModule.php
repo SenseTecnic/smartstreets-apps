a578ad2946ec0ddd61f43f5c3c5ec41c74132696
dyuen <daniellyyuen@hotmail.com>
2013-08-20 17:05:43 -0700

    fixed the view details javascript error

--- kurogo-1.8.3/sites/smartstreets/app/modules/CatalogueBrowser/CatalogueBrowserWebModule.php
+++ kurogo-1.8.3/sites/smartstreets/app/modules/CatalogueBrowser/CatalogueBrowserWebModule.php
@@ -16,7 +16,7 @@ class CatalogueBrowserWebModule extends WebModule
 
     protected function initializeForPage() {
 
-    	$this->addExternalJavascript('//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js');
+    	$this->addExternalJavascript('http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js');
     	$this->addExternalJavascript('http://code.jquery.com/ui/1.10.3/jquery-ui.js');
     	$this->addExternalCSS('http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css');
 

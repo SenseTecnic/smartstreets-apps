96423cab505435bde9c12e4bc100b2ce8822d0a8
Crystal Ng <crysng@dhcp-128-189-244-36.ubcsecure.wireless.ubc.ca>
2013-10-25 15:20:07 -0700

    fixed multiple junction issues

--- kurogo-1.8.3/sites/smartstreets/app/modules/TrafficExplorer/javascript/index-common.js
+++ kurogo-1.8.3/sites/smartstreets/app/modules/TrafficExplorer/javascript/index-common.js
@@ -959,14 +959,23 @@ $(document).ready(function() {
 							var allFlow = dataByDate.bottom(Infinity);
 
 							var flow =[];
+							var lat=null;
+							var lon=null;
 							for (var j in allFlow){
 								var x_value = new Date (allFlow[j]["tf_recordedtime"]*1000);
 								var totalFlow= allFlow[j]["tf_medflow"]+allFlow[j]["tf_longflow"]+allFlow[j]["tf_smallflow"]+allFlow[j]["tf_largeflow"];
 								var y_value = totalFlow;
+								if (lat==null || lon==null){
+									lat=allFlow[j]["tf_geo"]["coordinates"][1];
+									lon=allFlow[j]["tf_geo"]["coordinates"][0];
+								}
 								console.log("item id="+ allFlow[j]["tf_id"]);
 								console.log("item geo="+allFlow[j]["tf_geo"]["coordinates"][0]+ ", "+allFlow[j]["tf_geo"]["coordinates"][1]);
 								console.log("item datetime="+ x_value);
-								flow.push({x: x_value, y: y_value, speed:allFlow[j]["tf_avgspeed"], region:allFlow[j]["region"]});
+								if (allFlow[j]["tf_geo"]["coordinates"][0]==lon &&allFlow[j]["tf_geo"]["coordinates"][1]==lat){
+
+									flow.push({x: x_value, y: y_value, speed:allFlow[j]["tf_avgspeed"], region:allFlow[j]["region"]});
+								}
 							}
 							var value={};
 					        value["color"]= color_array[i];
